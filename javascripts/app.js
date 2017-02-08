3ee58085a574cbf509ef0754c7e1cad487e4e8b3
Ruud Greven <ruud@rgonline.nl>
2017-02-08 14:46:55 +0100

    Initial version

---
+++ javascripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name arcsplannerApp
+ * @description
+ * # arcsplanner
+ *
+ * Main module of the application.
+ */
+angular
+    .module('arcsplannerApp', []).constant('config', {
+
+    });
---
+++ javascripts/controllers/blocks.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name arcsplannerApp.controller:BlocksCtrl
+ * @description
+ * # BlocksCtrl
+ * Controller of the blocks
+ */
+angular.module('arcsplannerApp')
+    .controller('BlocksCtrl', function ($scope, $http, $log, config) {
+        $scope.blocks = [];
+
+        $http({
+            method: 'GET',
+            url: '/api/blocks'
+        }).then(function success(response) {
+            $scope.blocks = response.data.blocks;
+            $log.info("Retrieved blocks: " + $scope.blocks);
+        }, function error(response) {
+            $log.error("There was an error: ");
+        });
+    });
