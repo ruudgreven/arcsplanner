ee30d8404421e321856fd04e3acf23589ee37956
Ruud Greven <ruud@rgonline.nl>
2017-03-06 22:08:27 +0100

    Added UI router and basic filtering

--- www/javascripts/app.js
+++ www/javascripts/app.js
@@ -9,6 +9,29 @@
  * Main module of the application.
  */
 angular
-    .module('arcsplannerApp', ['ngSanitize', 'angular-timeline']).constant('config', {
+    .module('arcsplannerApp', [
+        'ngSanitize',
+        'angular-timeline',
+        'ui.router'
+    ]).config(function ($stateProvider) {
+        $stateProvider
+            .state('planner',{
+                url: '',
+                views: {
+                    'planner': {
+                        templateUrl: 'views/planner/planner.phtml',
+                        controller: 'PlannerCtrl'
+                    },
+                    'selector': {
+                        templateUrl: 'views/planner/selector.phtml',
+                        controller: 'BlocksCtrl',
+                    },
+                    'timeline': {
+                        templateUrl: 'views/planner/timeline.phtml',
+                        controller: 'TimelineCtrl'
+                    }
+                }
+            });
+    }).constant('config', {
 
     });
--- www/javascripts/controllers/BlocksCtrl.js
+++ www/javascripts/controllers/BlocksCtrl.js
@@ -10,16 +10,49 @@
 angular.module('arcsplannerApp')
     .controller('BlocksCtrl', function ($scope, $http, $log, ConverterSvc, config) {
         $scope.blocks = [];
+        $scope.filter = {
+            text: undefined,
+            phase: undefined,
+            arcs:'',
+            grouping:''
+        };
 
-        $http({
-            method: 'GET',
-            url: '/api/blocks'
-        }).then(function success(response) {
-            $scope.blocks = response.data.blocks;
-            $log.info("Retrieved blocks: " + $scope.blocks);
-        }, function error(response) {
-            $log.error("There was an error: " + response);
-        });
+        /**
+         * Constructuro for the blockscontroller. Receive the blocks from the server
+         */
+        $scope.init = function() {
+            $http({
+                method: 'GET',
+                url: '/api/blocks'
+            }).then(function success(response) {
+                $scope.blocks = response.data.blocks;
+                $log.info("Retrieved blocks: " + $scope.blocks);
+            }, function error(response) {
+                $log.error("There was an error: " + response);
+            });
+        };
+
+        $scope.setFilter = function(name, filtervalue) {
+            if (name == 'text') {
+                $scope.filter.text = filtervalue;
+            } else if (name == 'phase') {
+                $scope.filter.phase = filtervalue;
+            } else if (name == 'arcs') {
+                $scope.filter.arcs = filtervalue;
+            } else if (name == 'grouping') {
+                $scope.filter.grouping = filtervalue;
+            }
+        };
+
+        $scope.applyFilter = function(block) {
+            if ($scope.filter.text != undefined && block.title.indexOf($scope.filter.text) == -1) {
+                return false;
+            }
+            if ($scope.filter.phase != undefined && block.phases.indexOf($scope.filter.phase) == -1) {
+                return false;
+            }
+            return true;
+        };
 
         $scope.convertToHtml = function(text) {
             return ConverterSvc.convertToHtml(text);
