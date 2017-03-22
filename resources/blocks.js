8d27f47d975987cb89e397acce39306c8ecb1c9a
Ruud Greven <ruud@rgonline.nl>
2017-03-22 10:07:14 +0100

    Updated for running without a webserver and added blocks

--- resources/blocks.js
+++ resources/blocks.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res) {
     res.type('application/json');
-    res.sendFile('blocks.json', {root: '../arcsplanner'});      //???
+    res.sendFile('www/blocks.json', {root: '../arcsplanner'});      //???
 });
 
 module.exports = router;
